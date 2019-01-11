b5b646c74f44aadcd3add6a5de4bf80fa45a5228
Victor Tsang <vtsang@netbase.com>
2019-01-10 23:14:25 -0800

    excersise but using emotions styled components

--- src/components/UI/Navbar/Navbar.js
+++ src/components/UI/Navbar/Navbar.js
@@ -1,5 +1,67 @@
 import React, { Component } from 'react';
-import './Navbar.css';
+import styled from '@emotion/styled';
+
+const Nav = styled.nav`
+	padding: 20px;
+	margin: 0 auto;
+	max-width: 1440px;
+
+	ul {
+		position: relative;
+		display: flex;
+		justify-content: space-evenly;
+		border-bottom: 2px solid #dbdbdb;
+		padding-bottom: 10px;
+		li {
+			display: inline-block;
+			a {
+				color: #b2b2b2;
+				letter-spacing: 1px;
+				font-size: 1em;
+				padding-bottom: 10px;
+				&:hover {
+					color: #6ca0c5;
+				}
+				&.active {
+					color: #1f1f1f;
+					&:hover {
+						color: #1f1f1f;
+						cursor: default;
+					}
+				}
+			}
+		}
+	}
+	@media only screen and (max-width: 980px) {
+		ul {
+			flex-direction: column;
+			border-bottom: none;
+			li {
+				padding: 15px 0;
+				a {
+					padding-bottom: 4px;
+					&.active {
+						border-bottom: 2px solid #1f1f1f;
+					}
+				}
+			}
+		}
+	}
+`;
+
+const Underline = styled.span`
+	position: absolute;
+	bottom: -2px;
+	height: 2px;
+	background-color: #1f1f1f;
+	transition: all 0.5s ease;
+	width: ${props => props.activeItemWidth}px;
+	left: ${props => props.activeItemLeft}px;
+
+	@media only screen and (max-width: 980px) {
+		display: none;
+	}
+`;
 
 class Navbar extends Component {
 	state = {
@@ -37,7 +99,7 @@ class Navbar extends Component {
 
 	render() {
 		return (
-			<nav className="navbar">
+			<Nav>
 				<ul>
 					{this.props.items.map((city, index) => {
 						return (
@@ -52,12 +114,12 @@ class Navbar extends Component {
 							</li>
 						);
 					})}
-					<span
-						className="underline"
-						style={{ width: this.state.activeItemWidth, left: this.state.activeItemLeft }}
+					<Underline
+						activeItemWidth={this.state.activeItemWidth}
+						activeItemLeft={this.state.activeItemLeft}
 					/>
 				</ul>
-			</nav>
+			</Nav>
 		);
 	}
 }
