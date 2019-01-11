3748779c07cfa2fe55b1abf25297b2cf9bf4e37c
Victor Tsang <vtsang@netbase.com>
2019-01-10 21:43:02 -0800

    Updates

---
+++ precache-manifest.4140c58765cd5cbce6a2a387abc879ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "593706314dc1bac113ec",
+    "url": "/citiesNavbarReact/static/js/runtime~main.59370631.js"
+  },
+  {
+    "revision": "44e4cfd79e2bf9d1293a",
+    "url": "/citiesNavbarReact/static/js/main.44e4cfd7.chunk.js"
+  },
+  {
+    "revision": "20864010ac12fc867baf",
+    "url": "/citiesNavbarReact/static/js/1.20864010.chunk.js"
+  },
+  {
+    "revision": "44e4cfd79e2bf9d1293a",
+    "url": "/citiesNavbarReact/static/css/main.6153b1c3.chunk.css"
+  },
+  {
+    "revision": "a7839b98894edcd83b567e960d7aaba7",
+    "url": "/citiesNavbarReact/index.html"
+  }
+];
